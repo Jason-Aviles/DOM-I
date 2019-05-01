5e6bcbe5cda5c830067e2da705a8210204e644d5
Jason Aviles <sandman2k18@outlook.com>
2019-05-01 19:27:12 -0400

    finnished

--- js/index.js
+++ js/index.js
@@ -38,5 +38,89 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
+
+//nav
+let nav = document.querySelectorAll('nav a')
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+nav[0].textContent = siteContent['nav']['nav-item-1'];
+nav[1].textContent = siteContent['nav']['nav-item-2'];
+nav[2].textContent = siteContent['nav']['nav-item-3'];
+nav[3].textContent = siteContent['nav']['nav-item-4'];
+nav[4].textContent = siteContent['nav']['nav-item-5'];
+nav[5].textContent = siteContent['nav']['nav-item-6'];
+
+//cta
+let banner = document.getElementById('cta-img');
+let ctaText = document.querySelector('.cta-text h1');
+let ctaBtn =document.querySelector('.cta-text button');
+
+
+banner.setAttribute('src',siteContent['cta']['img-src']);
+ctaText.textContent= siteContent['cta']['h1'];
+ctaBtn.textContent = siteContent['cta']['button']
+
+
+//main-contact
+
+let topContent = document.querySelectorAll(`.top-content .text-content `);
+let bottomContent = document.querySelectorAll('.bottom-content .text-content')
+
+let topContentArr =Array.from(topContent)
+let bottomContentArr = Array.from(bottomContent)
+
+//features h4 and p
+topContentArr[0].children[0].textContent= siteContent['main-content']['features-h4'];
+topContentArr[0].children[1].textContent= siteContent['main-content']['features-content'];
+
+
+//About h4 and p
+topContentArr[1].children[0].textContent= siteContent['main-content']['about-h4']
+topContentArr[1].children[1].textContent= siteContent['main-content']['about-content']
+
+
+//middle img
+let middleImg = document.getElementById('middle-img');
+
+middleImg.setAttribute(['src'],siteContent['main-content']['middle-img-src'])
+//=========================================================================================
+
+//services
+bottomContentArr[0].children[0].textContent = siteContent['main-content']['services-h4'];
+bottomContentArr[0].children[1].textContent = siteContent['main-content']['services-content'];
+
+//product
+
+bottomContentArr[1].children[0].textContent = siteContent['main-content']['product-h4'];
+bottomContentArr[1].children[1].textContent = siteContent['main-content']['product-content']
+
+
+//vision
+
+bottomContentArr[2].children[0].textContent = siteContent['main-content']['vision-h4'];
+bottomContentArr[2].children[1].textContent =siteContent['main-content']['vision-content']
+
+
+
+//===========================================================================================
+//contact
+let contact = document.querySelectorAll('.contact');
+
+let contactArr = Array.from(contact);
+
+let footer =document.querySelector('footer p')
+
+console.log(contactArr[0]);
+
+contactArr[0].children[0].textContent = siteContent['contact']['contact-h4'];
+contactArr[0].children[1].textContent = siteContent['contact']['address'];
+contactArr[0].children[2].textContent = siteContent['contact']['phone'];
+contactArr[0].children[3].textContent = siteContent['contact']['email'];
+
+//==========================================================================
+//footer
+
+footer.textContent = siteContent['footer']['copyright']
+
